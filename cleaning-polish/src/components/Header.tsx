fe0e46a6f78de280f6ee3f3df6d81baa27d552fb
Muzammil-haider <muzammilhaider17@gmail.com>
2025-05-03 22:05:30 +0500

    updated version

--- cleaning-polish/src/components/Header.tsx
+++ cleaning-polish/src/components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div className="flex flex-col md:flex-row md:justify-between items-center bg-[#FFF3E3] px-4 py-4 sticky top-0 z-50">
       <div className="flex items-center space-x-3">
         <Image className="rounded-lg" src="/images/professionalcleaning3.png" width={50} height={10} alt="logo" />
-        <div className="text-xl md:text-3xl text-black font-bold">Profesjonalne Sprzątanie</div>
+        <div className="text-xl md:text-3xl text-black font-bold">Blask Warszawa </div>
       </div>
       <div className="flex flex-wrap justify-center md:space-x-10 space-x-4 mt-4 md:mt-0 text-sm md:text-base font-light">
         <a href="#home" className="hover:text-[#B88E2F] transition">Strona Główna</a>
